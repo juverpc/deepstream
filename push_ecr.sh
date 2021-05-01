d65b646e62f8c02c4f42e9cc4c943a551e0f2d66
Jose Portillo Cruz <jose.portillo@globant.com>
2021-04-30 23:54:20 -0500

    update codebuil.yml

--- push_ecr.sh
+++ push_ecr.sh
@@ -2,9 +2,9 @@
 
 CSV=${1:-'tags_name.csv'}
 ECR_URL="624733648801.dkr.ecr.us-east-1.amazonaws.com"
-ECR_REPO_NAME="demo"
+ECR_REPO_NAME="deepstream-poc"
 
 for i in $(cat ${CSV} | grep -v '#')
 do
-docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
+docker push public.ecr.aws/s5w6j9m1/${ECR_REPO_NAME}:${i}
 done
--- tag.sh
+++ tag.sh
@@ -2,10 +2,10 @@
 
 CSV=${1:-'tags_name.csv'}
 ECR_URL="624733648801.dkr.ecr.us-east-1.amazonaws.com"
-ECR_REPO_NAME="deepstream"
+ECR_REPO_NAME="deepstream-poc"
 
 #for i in sagemaker-safety-image sagemaker-fnb-image 
 for i in $(cat ${CSV} | grep -v '#')
 do
-docker tag ${ECR_REPO_NAME}:${i} ${ECR_URL}/${ECR_REPO_NAME}:${i}
+docker tag ${ECR_REPO_NAME}:${i} public.ecr.aws/s5w6j9m1/${ECR_REPO_NAME}:${i}
 done
